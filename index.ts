4389d5cde1e189cb08c9cf23a7e3ff3ef3ad9a4e
unknown <mahelnguindja@gmail.com>
2025-08-15 17:36:06 +0100

    Premier commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { Resend } from 'resend';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const resend = new Resend('re_Zq2tDt23_JFzrSgTPiZCe5Kyp7unofZSc');
+
+function generateEmailHtml() {
+    try {
+        // Lire le contenu du fichier karmacbridges.html
+        const templatePath = join(__dirname, 'email-templates/devis-aurex-group.html');
+        const htmlContent = readFileSync(templatePath, 'utf-8');
+
+        // Remplacer la variable de date dynamique
+        return htmlContent.replace('${new Date().getFullYear()}', new Date().getFullYear().toString());
+    } catch (error) {
+        console.error('Erreur lors de la lecture du template HTML:', error);
+        return '<p>Erreur lors du chargement du template</p>';
+    }
+}
+
+async function sendEmail() {
+    const html = generateEmailHtml();
+    try {
+        const data = await resend.emails.send({
+            from: 'GytX Developer <developer@gytx.dev>',
+            to: ['mahelnguindja@gmail.com'],
+            subject: 'Devis Aurex Group',
+            html,
+        });
+        console.log('Email envoyé avec succès :', data);
+} catch (error: any) {
+        console.error('Erreur lors de l\'envoi de l\'email :', error?.message || error);
+    }
+}
+
+sendEmail();
